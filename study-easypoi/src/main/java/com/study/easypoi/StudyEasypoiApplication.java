df8cbf14cc0420f0315d57da9183f282d49fb944
zaft-x <zaft_x@163.com>
2020-12-10 14:21:16 +0800

    easy poi config

---
+++ study-easypoi/src/main/java/com/study/easypoi/StudyEasypoiApplication.java
@@ -0,0 +1,13 @@
+package com.study.easypoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEasypoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyEasypoiApplication.class, args);
+    }
+
+}
